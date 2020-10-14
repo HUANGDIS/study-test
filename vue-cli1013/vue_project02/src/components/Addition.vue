c597680771d3292d006649d188b7dfdf2bb3109f
huangpeipei <huangpeipei@inspur.com>
2020-10-14 17:57:28 +0800

    demo10141

--- vue-cli1013/vue_project02/src/components/Addition.vue
+++ vue-cli1013/vue_project02/src/components/Addition.vue
@@ -1,13 +1,22 @@
 <template>
 <div>
-  <h3>当前数目为：</h3>
-  <button>+1</button>
+  <h3>当前数目为: {{count }} </h3>
+  <button @click="handle1">+1</button>
 </div>
 </template>
 <script>
+import {mapState} from 'vuex'
 export default {
   data () {
     return {}
+  },
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    handle1: function () {
+      this.$store.commit('add') 
+    }
   }
 }
 </script>
--- vue-cli1013/vue_project02/src/store.js
+++ vue-cli1013/vue_project02/src/store.js
@@ -4,5 +4,12 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {}
+  state: {
+    count: 0
+  },
+  mutation: {
+    add(state){
+      state += 1
+    }
+  }
 })
